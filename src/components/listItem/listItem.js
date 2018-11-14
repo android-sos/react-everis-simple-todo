995a30e9cad5b102133119f5dca835f4a2eaf3b0
samuel <samuel.ospina36@gmail.com>
2018-11-13 23:07:56 -0500

    refactor

---
+++ src/components/listItem/listItem.js
@@ -0,0 +1,19 @@
+export React from 'react'
+
+const listItem = (props) => {
+    return (
+        <div>
+            <ul>
+            {
+                props.tasks.map(()=>{
+
+                })
+                
+            }
+            </ul>
+        </div>
+    );
+}
+
+export default listItem;
+
